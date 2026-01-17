df32edbcee5acdd861dc59e313031db9e34336b3
Icefighter156 <e12523112@student.tuwien.ac.at>
2026-01-17 16:51:01 +0100

    first changes

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { goto } from "$app/navigation";
+
+</script>
+
+
+<h1>Welcome to Miniprojekt 2</h1>
+
+<button on:click={goto("/question1")}>Click to start</button>
+
---
+++ src/routes/question1/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Fake or Real?</h1>
+
+<div class="buttonDiv">
+<button>Real</button>
+<button>Fake</button>
+</div>
+
+<img src="/images/email1.png" width="700">
+
